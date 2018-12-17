8f7a6cd46e7b3acc8a42675ba3c2255c9235cc7e
zgyang <zgyang@ittx.com.cn>
2018-12-17 20:17:36 +0800

    push

--- AutoCommit.sh
+++ AutoCommit.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #建议添加进定时任务
 #* * * * * source /root/some_bashscript/AutoCommit.sh && echo "last update success:`date`">>/tmp/Autocommit.txt || echo "last update false:`date`">>/tmp/Autocommit.txt
-gitplace='/opt/docker/jupyter/data/jupyter  /root/jupyter/data/jupyter /root/some_bashscript'
+gitplace='/opt/docker/jupyter/data/jupyter/  /root/jupyter/data/jupyter /root/some_bashscript/'
 for i in ${gitplace};do
 	if [ -d ${i} ];then
 
 		echo "开始pull $i"
 		cd ${i} && git pull --no-edit
 		echo "开始push $i"
-		cd ${i} && git add ${i} && git commit -m "automatic commit and push files" && git push ||git push
+		cd ${i} && git add . && git commit -m "automatic commit and push files" && git push ||git push
 	fi
 done
 
