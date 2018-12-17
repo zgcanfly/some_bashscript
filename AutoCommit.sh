d2ac437e47b5e5efed0d8c700d9e65dda7938c07
zgyang <zgyang@ittx.com.cn>
2018-12-17 12:44:45 +0800

    change commit message

--- AutoCommit.sh
+++ AutoCommit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 gitplace=''
-cd gitplace && git add gitplace && git commit -m "Auto commit and push files"&& git push
+cd gitplace && git add gitplace && git commit -m "automatic commit and push files"&& git push
