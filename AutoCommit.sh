50bea2aa2fd012b3a5a1ec3e9e230e6387bdb6f9
zgyang <zgyang@ittx.com.cn>
2018-12-17 13:20:37 +0800

    update bashscript

--- AutoCommit.sh
+++ AutoCommit.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 gitplace='/opt/docker/jupyter/data/jupyter'
+
+
+#开始pull
+cd ${gitplace} && git pull --no-edit
+#开始push
 cd ${gitplace} && git add ${gitplace} && git commit -m "automatic commit and push files"&& git push
