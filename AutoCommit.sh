e6400fbc57d47e0a9e3f5b586af6f7aabece28ba
zgyang <zgyang@ittx.com.cn>
2018-12-17 13:27:49 +0800

    可对多个目录操作

--- AutoCommit.sh
+++ AutoCommit.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-gitplace='/opt/docker/jupyter/data/jupyter'
+gitplace='/opt/docker/jupyter/data/jupyter  /root/jupyter/data/jupyter'
 
+for i in ${gitplace};do
+	#开始pull
+	cd ${gitplace} && git pull --no-edit
+	#开始push
+	cd ${gitplace} && git add ${gitplace} && git commit -m "automatic commit and push files"&& git push
+done
 
-#开始pull
-cd ${gitplace} && git pull --no-edit
-#开始push
-cd ${gitplace} && git add ${gitplace} && git commit -m "automatic commit and push files"&& git push
